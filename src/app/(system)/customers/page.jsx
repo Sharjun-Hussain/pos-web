96ba8c41eca4b588d5920b848ae03e4d4de4c87f
sharjun hussain <sharjoonibnuhussain99@gmail.com>
2025-10-19 22:09:26 +0530

    feat:added customers page

---
+++ src/app/(system)/customers/page.jsx
@@ -0,0 +1,8 @@
+import { CustomersManagement } from "@/components/customers/customers-management";
+import React from "react";
+
+const CustomerPage = () => {
+  return <CustomersManagement />;
+};
+
+export default CustomerPage;
--- src/components/page-header.jsx
+++ src/components/page-header.jsx
@@ -2,15 +2,17 @@ import { SidebarTrigger } from "./ui/sidebar";
 
 export function PageHeader({ title, description, action }) {
   return (
-    <div className="flex items-center justify-start gap-3 border-b border-border bg-card pl-3 py-6">
+    <div className="flex items-center gap-3 border-b border-border bg-card pl-3 py-6">
       <SidebarTrigger />
-      <div>
-        <h1 className="text-3xl font-bold tracking-tight">{title}</h1>
-        {description && (
-          <p className="mt-1 text-sm text-muted-foreground">{description}</p>
-        )}
+      <div className="flex flex-1 flex-row  mr-8 justify-between items-center">
+        <div>
+          <h1 className="text-3xl font-bold tracking-tight">{title}</h1>
+          {description && (
+            <p className="mt-1 text-sm text-muted-foreground">{description}</p>
+          )}
+        </div>
+        {action && <div>{action}</div>}
       </div>
-      {action && <div>{action}</div>}
     </div>
   );
 }
