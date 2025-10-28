307c6b4f3b490fae0723c7f02c331e57bcf9368c
sharjun hussain <sharjoonibnuhussain99@gmail.com>
2025-10-28 14:55:40 +0530

    feat:added meta description for branches so the title of the tab will user friendly

--- src/app/(system)/branches/page.jsx
+++ src/app/(system)/branches/page.jsx
@@ -3,3 +3,8 @@ import BranchesManagement from "@/components/branches/branches-management";
 export default function BranchesPage() {
   return <BranchesManagement />;
 }
+
+export const metadata = {
+  title: "Branches | EMI-POS  ",
+  description: "Developed By : Inzeedo (PVT) Ltd.",
+};
