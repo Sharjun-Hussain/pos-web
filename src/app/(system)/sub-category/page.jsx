2448017525ad0af4343d45aaf046132b03a1e151
sharjun hussain <sharjoonibnuhussain99@gmail.com>
2025-11-02 13:48:15 +0530

    feat:sub category api connection succesfull

--- src/app/(system)/sub-category/page.jsx
+++ src/app/(system)/sub-category/page.jsx
@@ -1,7 +1,13 @@
+import SubCategoryPage from "@/components/sub-category/sub-category-management";
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return <SubCategoryPage />;
 };
 
 export default page;
+
+export const metadata = {
+  title: "Sub Category | EMI-POS  ",
+  description: "Developed By : Inzeedo (PVT) Ltd.",
+};
